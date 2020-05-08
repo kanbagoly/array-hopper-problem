526f35effe7ccb52ffc5d9ec8ec718bc900fa468
Kanbagoly <kanbagoly@gmail.com>
2020-05-08 21:25:57 +0200

    Add failing test

---
+++ src/main/scala/com/kanbagoly/arrayhopper/ArrayHopper.scala
@@ -0,0 +1,49 @@
+package com.kanbagoly.arrayhopper
+
+object ArrayHopper {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  /*
+    private static List<Integer> findHops(List<Integer> numbers) {
+        if (numbers.isEmpty())
+            return Collections.emptyList();
+        List<Integer> hops = new ArrayList<>(Collections.singletonList(0));
+        int from = 0;
+        while (from + numbers.get(from) < numbers.size() && numbers.get(from) != 0) {
+            List<Integer> nextPlaces = numbers.subList(from + 1, Math.min(from + numbers.get(from) + 1, numbers.size()));
+            from += findNextHop(nextPlaces) + 1;
+            hops.add(from);
+        }
+        return from + numbers.get(from) >= numbers.size() ? hops : Collections.emptyList();
+    }
+
+      private static Integer findNextHop(List<Integer> numbers) {
+        int max = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max + numbers.get(max) < i + numbers.get(i)) {
+                max = i;
+            }
+        }
+        return max;
+    }*/
+
+  def findHops(numbers: List[Int]): List[Int] = numbers match {
+    case Nil => Nil
+    case 0::_ => Nil
+    case _::_ => List(0)
+  }
+
+  private def findHops2(numbers: List[Int], base: Int, hops: List[Int]): List[Int] = numbers match {
+    case Nil => hops
+    case x::xs =>
+      val hop = findMaxIndexPlusValue(xs.slice(0, (base + x).max(xs.size)))
+      findHops2(xs, base + hop, base :: hops.drop(hop))
+  }
+
+  private def findMaxIndexPlusValue(numbers: List[Int]): Int =
+    numbers.zipWithIndex.maxBy { case (v, i) => v + i }._1
+
+}
---
+++ src/test/scala/com/kanbagoly/arrayhopper/ArrayHopperSpec.scala
@@ -0,0 +1,32 @@
+package com.kanbagoly.arrayhopper
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class ArrayHopperSpec extends AnyWordSpecLike with Matchers {
+
+  "array hopper" should {
+    "return no indices" when {
+      "the array is empty" in {
+        ArrayHopper.findHops(Nil) should be(empty)
+      }
+      "the array only contains [0]" in {
+        ArrayHopper.findHops(List(0)) should be(empty)
+      }
+    }
+    "return all indices" when {
+      "the array contains one element of [1]" in {
+        ArrayHopper.findHops(List(1)) should be(List(0))
+      }
+      "the array contains more element of [1, 1, 1]" in {
+        ArrayHopper.findHops(List(1, 1, 1)) should be(List(0, 1, 2))
+      }
+    }
+//    "return indices of [0, 5, 9]" when {
+//      "the array contains the given example of [5, 6, 0, 4, 2, 4, 1, 0, 0, 4]" in {
+//        ArrayHopper.findHops(List(5, 6, 0, 4, 2, 4, 1, 0, 0, 4)) should be(List(0, 5, 9))
+//      }
+//    }
+  }
+
+}

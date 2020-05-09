c335b17a8743b77ea04a3e77e188c787d4d514f7
Kanbagoly <kanbagoly@gmail.com>
2020-05-09 21:36:30 +0200

    Simplify tests

--- src/test/scala/com/kanbagoly/arrayhopper/ArrayHopperSpec.scala
+++ src/test/scala/com/kanbagoly/arrayhopper/ArrayHopperSpec.scala
@@ -11,54 +11,50 @@ class ArrayHopperSpec extends AnyWordSpecLike with Matchers {
   "array hopper" should {
     "return no indices" when {
       "the array is empty" in {
-        val stdin = new StringReader("")
-        val stdout = execute(stdin)
-        stdout.toString should include("failure")
+        findHops() should startWith("failure")
       }
       "the array only contains [0]" in {
-        val stdin = new StringReader("0")
-        val stdout = execute(stdin)
-        stdout.toString should include("failure")
+        findHops(0) should startWith("failure")
       }
       "the array has an unjumpable gap like [1, 0, 1]" in {
-        ArrayHopper.findHops(List(1, 0, 1)) should be(empty)
+        findHops(1, 0, 1) should startWith("failure")
       }
     }
     "return all indices" when {
       "the array contains one element of [1]" in {
-        ArrayHopper.findHops(List(1)) should be(List(0))
+        findHops(1) should startWith("0, out")
       }
       "the array contains more elements of [1, 1, 1]" in {
-        ArrayHopper.findHops(List(1, 1, 1, 1, 1)) should be(List(0, 1, 2, 3, 4))
+        findHops(1, 1, 1) should startWith("0, 1, 2, out")
       }
       "the array contains lot of ones of [1, 1, 1, 1, 1]" in {
-        // TODO: Delete
-        ArrayHopper.findHops(List(1, 1, 1, 1, 1)) should be(List(0, 1, 2, 3, 4))
+        findHops(1, 1, 1, 1, 1) should startWith("0, 1, 2, 3, 4, out")
       }
     }
     "return appropriate indices" when {
       "the array has a jumpable gap like [2, 0, 1]" in {
-        ArrayHopper.findHops(List(2, 0, 1)) should be(List(0, 2))
+        findHops(2, 0, 1) should startWith("0, 2, out")
       }
       "the array has bigger jumpable gap then the size of the rest of the array [2, 0]" in {
-        ArrayHopper.findHops(List(2, 0)) should be(List(0))
+        findHops(2, 0) should startWith("0, out")
       }
       "the array has a better element to choose than to jump a maximum like [2, 3, 1, 1, 1]" in {
-        ArrayHopper.findHops(List(2, 3, 1, 1, 1)) should be(List(0, 1, 4))
+        findHops(2, 3, 1, 1, 1) should startWith("0, 1, 4, out")
       }
       "the array contains the given example of [5, 6, 0, 4, 2, 4, 1, 0, 0, 4]" in {
-        ArrayHopper.findHops(List(5, 6, 0, 4, 2, 4, 1, 0, 0, 4)) should be(List(0, 5, 9))
+        findHops(5, 6, 0, 4, 2, 4, 1, 0, 0, 4) should startWith("0, 5, 9, out")
       }
     }
   }
 
-  private def execute(in: StringReader): ByteArrayOutputStream = {
+  private def findHops(array: Int*): String = {
     val out = new ByteArrayOutputStream()
     Console.withOut(out) {
-      Console.withIn(in) {
+      Console.withIn(new StringReader(array.mkString(System.lineSeparator))) {
         ArrayHopper.main(Array.empty[String])
       }
     }
-    out
+    out.toString
   }
+
 }

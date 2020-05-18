6768f27dc91286c30ba075162880b2b03e8fca84
kanbagoly <54419616+kanbagoly@users.noreply.github.com>
2020-05-18 12:52:28 +0200

    Solution (#1)
    Add solution to the problem

---
+++ src/main/scala/com/kanbagoly/arrayhopper/ArrayHopper.scala
@@ -0,0 +1,45 @@
+package com.kanbagoly.arrayhopper
+
+import scala.annotation.tailrec
+import scala.io.StdIn
+
+object ArrayHopper {
+
+  def main(args: Array[String]): Unit = {
+    val flights = readNumbers().partitionMap(identity) match {
+      case (_, Nil) => Nil
+      case (Nil, numbers) => findHops(numbers)
+      case (_, _) => Nil
+    }
+    println(prepareOutput(flights))
+  }
+
+  @tailrec
+  private def findHops(numbers: List[Int], acc: List[Int] = Nil, offset: Int = 0): List[Int] = numbers match {
+    case 0::_ => Nil
+    case x::xs if xs.size < x => (offset :: acc).reverse
+    case x::xs =>
+      val jump = findMaxIndexPlusValue(xs take x)
+      findHops(xs drop jump, offset :: acc, offset + jump + 1)
+  }
+
+  private def findMaxIndexPlusValue(numbers: List[Int]): Int =
+    numbers.zipWithIndex.maxBy { case (v, i) => v + i }._2
+
+  private def readNumbers(): List[Either[NumberFormatException, Int]] =
+    Iterator.continually(StdIn.readLine()).takeWhile(_ != null).map(toNonNegativeInt).toList
+
+  private def toNonNegativeInt(number: String): Either[NumberFormatException, Int] =
+    try {
+      val int = number.toInt
+      if (int < 0) Left(new NumberFormatException("negative number")) else Right(int)
+    } catch {
+      case nfe: NumberFormatException => Left(nfe)
+    }
+
+  private def prepareOutput(numbers: List[Int]): String = numbers match {
+    case Nil => "failure"
+    case xs => xs.mkString("", ", ", ", out")
+  }
+
+}
---
+++ src/test/scala/com/kanbagoly/arrayhopper/ArrayHopperSpec.scala
@@ -0,0 +1,66 @@
+package com.kanbagoly.arrayhopper
+
+import java.io.{ByteArrayOutputStream, StringReader}
+import java.lang.System.lineSeparator
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+
+class ArrayHopperSpec extends AnyWordSpecLike with Matchers {
+
+  "array hopper" should {
+    "return no indices" when {
+      "the array is empty" in {
+        findHops() should be("failure")
+      }
+      "the array contains negative number" in {
+        findHops(-1) should be("failure")
+      }
+      "the array contains a non integer" in {
+        findHops("NaN") should be("failure")
+      }
+      "the array only contains [0]" in {
+        findHops(0) should be("failure")
+      }
+      "the array has an unjumpable gap like [1, 0, 1]" in {
+        findHops(1, 0, 1) should be("failure")
+      }
+    }
+    "return all indices" when {
+      "the array contains one element of [1]" in {
+        findHops(1) should be("0, out")
+      }
+      "the array contains more elements of [1, 1, 1]" in {
+        findHops(1, 1, 1) should be("0, 1, 2, out")
+      }
+      "the array contains lot of ones of [1, 1, 1, 1, 1]" in {
+        findHops(1, 1, 1, 1, 1) should be("0, 1, 2, 3, 4, out")
+      }
+    }
+    "return appropriate indices" when {
+      "the array has a jumpable gap like [2, 0, 1]" in {
+        findHops(2, 0, 1) should be("0, 2, out")
+      }
+      "the array has bigger jumpable gap then the size of the rest of the array [2, 0]" in {
+        findHops(2, 0) should be("0, out")
+      }
+      "the array has a better element to choose than to jump a maximum like [2, 3, 1, 1, 1]" in {
+        findHops(2, 3, 1, 1, 1) should be("0, 1, 4, out")
+      }
+      "the array contains the given example of [5, 6, 0, 4, 2, 4, 1, 0, 0, 4]" in {
+        findHops(5, 6, 0, 4, 2, 4, 1, 0, 0, 4) should be("0, 5, 9, out")
+      }
+    }
+  }
+
+  private def findHops(array: Any*): String = {
+    val out = new ByteArrayOutputStream()
+    Console.withOut(out) {
+      Console.withIn(new StringReader(array.mkString(lineSeparator))) {
+        ArrayHopper.main(Array.empty[String])
+      }
+    }
+    out.toString.replace(lineSeparator(), "")
+  }
+}

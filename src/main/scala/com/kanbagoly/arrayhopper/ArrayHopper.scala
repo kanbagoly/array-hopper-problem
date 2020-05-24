94f7ecbf6f61e5a1ed32d49ad51a5164e677d504
Kanbagoly <kanbagoly@gmail.com>
2020-05-25 01:42:58 +0200

    Modify algorithm

--- src/main/scala/com/kanbagoly/arrayhopper/ArrayHopper.scala
+++ src/main/scala/com/kanbagoly/arrayhopper/ArrayHopper.scala
@@ -2,11 +2,12 @@ package com.kanbagoly.arrayhopper
 
 import scala.annotation.tailrec
 import scala.io.StdIn
+import scala.util.Try
 
 object ArrayHopper {
 
   def main(args: Array[String]): Unit = {
-    val flights = readNumbers().partitionMap(identity) match {
+    val flights = readNumbers().partitionMap(_.toEither) match {
       case (Nil, numbers@_::_) => findHops(numbers)
       case _ => Nil
     }
@@ -16,25 +17,25 @@ object ArrayHopper {
   @tailrec
   private def findHops(numbers: List[Int], acc: List[Int] = Nil, offset: Int = 0): List[Int] = numbers match {
     case 0::_ => Nil
-    case x::xs if xs.size < x => (offset :: acc).reverse
     case x::xs =>
-      val jump = findMaxIndexPlusValue(xs take x)
-      findHops(xs drop jump, offset :: acc, offset + jump + 1)
+      val horizon = xs take x
+      if (horizon.size < x) (offset :: acc).reverse
+      else {
+        val jump = findMaxIndexPlusValue(horizon)
+        findHops(xs drop jump, offset :: acc, offset + jump + 1)
+      }
   }
 
   private def findMaxIndexPlusValue(numbers: List[Int]): Int =
     numbers.zipWithIndex.maxBy { case (v, i) => v + i }._2
 
-  private def readNumbers(): List[Either[NumberFormatException, Int]] =
+  private def readNumbers(): List[Try[Int]] =
     Iterator.continually(StdIn.readLine()).takeWhile(_ != null).map(toNonNegativeInt).toList
 
-  private def toNonNegativeInt(number: String): Either[NumberFormatException, Int] =
-    try {
-      val int = number.toInt
-      if (int < 0) Left(new NumberFormatException("negative number")) else Right(int)
-    } catch {
-      case nfe: NumberFormatException => Left(nfe)
-    }
+  private def toNonNegativeInt(number: String): Try[Int] = Try { requireNonNegative(number.toInt) }
+
+  private def requireNonNegative(number: Int): Int =
+    if (number < 0) throw new NumberFormatException("negative number") else number
 
   private def prepareOutput(numbers: List[Int]): String = numbers match {
     case Nil => "failure"
